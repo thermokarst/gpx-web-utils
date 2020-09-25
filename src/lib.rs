1d04a7139f33b0c661d61656d30cd4fa7b89e58a
Matthew Dillon <matthewrdillon@gmail.com>
2020-09-25 11:00:05 -0700

    cargo cleanup; remove wee allocator; license

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,6 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[wasm_bindgen]
 extern {
     fn alert(s: &str);
@@ -15,5 +9,7 @@ extern {
 
 #[wasm_bindgen]
 pub fn greet() {
+    utils::set_panic_hook();
+
     alert("Hello, gpx-web-utils!");
 }
