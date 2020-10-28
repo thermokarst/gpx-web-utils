22b2db956411a02cd90dae75240fc1cbe657d893
thermokarst <thermokarst@users.noreply.github.com>
2020-10-28 16:34:06 +0000

    deploy: 5b7fc59257a554e6c31058c7429d3ba4a3807886

--- bootstrap.js
+++ bootstrap.js
@@ -171,7 +171,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/gpx_web_utils_bg.wasm":"46d4844c9de40702e69e"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/gpx_web_utils_bg.wasm":"29d4260a78fb3b234229"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
