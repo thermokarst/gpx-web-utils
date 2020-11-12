ec2651207cedd3834204107a95f47c6bc55b11ae
Matthew Dillon <matthewrdillon@gmail.com>
2020-11-12 16:09:28 -0700

    Initial implementation (#4)

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,16 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-pub fn greet() {
+pub fn merge(files: js_sys::Array) -> wasm_bindgen::JsValue {
     utils::set_panic_hook();
+    let files: Vec<String> = utils::translate_js_to_rust(files);
+    let merged: gpx::Gpx = utils::join_gpx_files(files);
+    let out_vec: Vec<u8> = utils::write_gpx_to_buffer(merged);
 
-    alert("Hello, gpx-web-utils!");
+    JsValue::from_str(&String::from_utf8(out_vec).unwrap())
 }
--- www/index.js
+++ www/index.js
@@ -1,3 +1,27 @@
-import * as wasm from "gpx-web-utils";
+import * as gpx from "gpx-web-utils";
 
-wasm.greet();
+const inputElement = document.getElementById("input");
+
+inputElement.addEventListener("change", readFiles, false);
+
+function readFiles() {
+  if (inputElement.files.length < 2) { alert("open two or more files"); return; }
+
+  const files = Array.from(inputElement.files);
+  const promises = files.map(f => f.text());
+
+  Promise.all(promises).then(gpxes => {
+    const merged = gpx.merge(gpxes);
+    writeOutput(merged);
+    inputElement.value = "";
+  });
+}
+
+function writeOutput(file) {
+    const blob = new Blob([file], {type: "text/gpx"});
+    const a = document.createElement("a");
+    a.href = URL.createObjectURL(blob);
+    a.download = "merged.gpx";
+    a.click();
+    URL.revokeObjectURL(a.href);
+}
