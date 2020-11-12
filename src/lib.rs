1432344dbed05ca9eb4f8a2800e69bde5d2588f5
Matthew Dillon <matthewrdillon@gmail.com>
2020-11-12 15:25:17 -0700

    squash

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 pub fn merge(files: js_sys::Array) -> wasm_bindgen::JsValue {
     utils::set_panic_hook();
 
-    let files = utils::translate_js_to_rust(files);
+    let files: Vec<String> = utils::translate_js_to_rust(files);
     let merged: gpx::Gpx = utils::join_gpx_files(files);
     let out_vec: Vec<u8> = utils::write_gpx_to_buffer(merged);
     JsValue::from_str(&String::from_utf8(out_vec).unwrap())
