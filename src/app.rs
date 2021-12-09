03d6a56b127958cdaafdbbbfd903ad8f778fb224
Matthew Dillon <matthewrdillon@gmail.com>
2021-12-09 07:54:22 -0700

    wip

--- src/app.rs
+++ src/app.rs
@@ -13,18 +13,26 @@ impl Component for App {
     }
 
     fn view(&self, _ctx: &Context<Self>) -> Html {
+        let topografix = move || -> Html {
+            html! { <a href="https://www.topografix.com/gpx.asp">{"GPX files"}</a> }
+        };
+
+        let garmin = move || -> Html {
+            html! {<a href="https://www.garmin.com">{"Garmin"}</a>}
+        };
+
+        let strava = move || -> Html {
+            html! {<a href="https://www.strava.com">{"Strava"}</a>}
+        };
+
         html! {
             <>
                 <h1>
-                   <a href="/">{"gpx.thermokar.st"}</a>
+                   <a href="/">{ "gpx.thermokar.st" }</a>
                 </h1>
 
                 <p>
-                    {"This client-side tool is for merging "}
-                    <a href="https://www.topografix.com/gpx.asp">
-                        {"GPX files"}
-                    </a>
-                    {". "}
+                    { "A client-side tool for merging "}{ topografix() }{ "." }
                 </p>
 
                 <Loader />
@@ -32,21 +40,14 @@ impl Component for App {
                 <hr/>
 
                 <p>
-                    {"Note, this has only been tested on GPX files produced by "}
-                    <a href="https://www.garmin.com">
-                        {"Garmin"}
-                    </a>
-                    {" and "}
-                    <a href="https://www.strava.com">
-                        {"Strava"}
-                    </a>
-                    {" - your mileage may vary."}
+                    { "Note, this has only been tested on GPX files produced by " }
+                    { garmin() }{ " and " }{ strava() }{ " - your mileage may vary." }
                 </p>
 
                 <p>
                     <small>
-                        {"source (public access): "}
-                        {"git://pingo.thermokar.st/gpx-web-utils"}
+                        { "source (public access): " }
+                        { "git://pingo.thermokar.st/gpx-web-utils" }
                     </small>
                 </p>
             </>
